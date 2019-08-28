43b91924fef89f672199b346206afa56cea6a872
sam-malanchuk <malanchuksam@gmail.com>
2019-08-27 20:14:38 -0400

    created and tested database endpoint for findSteps()

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -10,15 +10,27 @@ module.exports = {
 };
 
 function find() {
+    // SELECT * FROM Schemes
     return db('schemes');
 }
 
 function findById(id) {
+    // SELECT * FROM Schemes
+    // WHERE id=id
     return db('schemes').where({id}).first();
 }
 
-function findSteps() {
-
+function findSteps(id) {
+    // SELECT id, scheme_name, step_number, instructions FROM Steps AS T
+    // JOIN schemes as S ON T.scheme_id = S.id
+    // WHERE scheme_id=id
+    // ORDER BY step_number
+    return db
+        .from('Steps as T')
+        .join('Schemes as S', 'T.scheme_id', 'S.id')
+        .select('T.id', 'S.scheme_name', 'T.step_number', 'T.instructions')
+        .where({ scheme_id: id })
+        .orderBy('step_number', 'asc')
 }
 
 function add() {
